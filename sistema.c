1c183b8fc0b260ea8859b4119a53606efe544635
dalferr <da7172.da@gmail.com>
2024-10-24 00:06:45 +0200

    Reloj funcionando desordenado

--- sistema.c
+++ sistema.c
@@ -3,12 +3,13 @@
 
 // Definir variables de sincronización
 pthread_mutex_t mutex;
+pthread_cond_t cond;
 pthread_cond_t cond1;
 pthread_cond_t cond2;
 
 // Definir variables globales
 int done = 0;
-int tenp_kont=2;
+int tenp_kont = 2;
 
 // Estructura de PCB
 typedef struct {
@@ -30,6 +31,12 @@ typedef struct {
 } machine;
 
 
+void* sche_dispa(void* arg) {
+  printf("Se ha llamado al Scheduler\n");
+  return NULL;
+}
+
+
 // FUncion clock
 void* reloj(void* arg) {
   while(1){
@@ -45,51 +52,61 @@ void* reloj(void* arg) {
 
 
 /// Funcion timer
-void* timer(int freq) {
+void* timer(void* arg) {
+ // int freq = *((int*)arg); // Cast para convertir void* en puntero
   pthread_mutex_lock(&mutex);
   while(1){
     done++;
     
-    //Añadir auqui lo que hace
-    for (int i=0;i<freq;i++) {
+    //sche_dispa(NULL);
+    printf("Ciclo en el timer\n");
 
-    }
-
-    pthread_cond_signal(&cond1);
+    pthread_cond_signal(&cond1); 
     pthread_cond_wait(&cond2,&mutex);
+    //pthread_mutex_unlock(&mutex);
   }
 }
 
 
 // Funcion Process Generator
-void* process_gen(int freq) {
+void* process_gen(void* arg) {
+  //int freq = *((int*)arg); // Cast para convertir void* en puntero
   pthread_mutex_lock(&mutex);
   while(1){
     done++;
 
-    for (int i=0;i<freq;i++) {
 
-    }
+    printf("Ciclo en el Process Generator\n");
 
     pthread_cond_signal(&cond1);
     pthread_cond_wait(&cond2,&mutex);
+    //pthread_mutex_unlock(&mutex);
   }
 }
 
-// FUncion clock
-void* sche_dispa(void* arg) {
-  printf("Se ha llamado al Scheduler\n");
-}
 
-int main(int freq_temp, int freq_sche, int freq_PCB, int hilos, int cores, int CPUs) {
+
+int main(int argc, char* argv[]) {
+
+  //Iniciamos las variables de los hilos
+  pthread_mutex_init(&mutex, NULL);
+  pthread_cond_init(&cond, NULL);
+  pthread_cond_init(&cond1, NULL);
+  pthread_cond_init(&cond2, NULL);
 
   // Creamos los hilos
   pthread_t th_clock;
   pthread_t th_timer;
   pthread_t th_process_gen;
   pthread_t th_sche_dispa;
-  pthread_create(&th_clock, NULL, clock, NULL);
+  pthread_create(&th_clock, NULL, reloj, NULL);
   pthread_create(&th_timer, NULL, timer, NULL);
   pthread_create(&th_process_gen, NULL, process_gen, NULL);
   pthread_create(&th_sche_dispa, NULL, sche_dispa, NULL);
+
+  // Esperar a que los hilos se junten
+  pthread_join(th_clock, NULL);
+  pthread_join(th_timer, NULL);
+  pthread_join(th_process_gen, NULL);
+  pthread_join(th_sche_dispa, NULL);
 }
