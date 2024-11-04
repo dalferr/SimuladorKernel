79b5ca4184220b6cdd3880c2bd4b30270ce69f15
dalferr <da7172.da@gmail.com>
2024-11-04 16:44:45 +0100

    Separado en partes

---
+++ structs.h
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <pthread.h>
+#include <stdlib.h>
+#include <time.h>
+
+
+// Estructura de PCB
+typedef struct {
+  int pid;
+} PCB;
+
+// Estructura de Cola de Procesos
+typedef struct {
+  PCB queue[100000];
+  int ini;
+  int fin;
+} process_queue;
+
+process_queue cola_procesos = {0, -1};
+
+// Estructura Machine
+typedef struct {
+  int CPUs;
+  int cores;
+  int hilos;
+} machine;
